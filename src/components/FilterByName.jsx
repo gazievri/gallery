7ce133f6348e1a3a38219e9c1c6d446e66012504
Ruslan Gaziev <grinrus@yandex.ru>
2022-08-11 01:33:47 +0300

    Se

--- src/components/FilterByName.jsx
+++ src/components/FilterByName.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Filter = ({ handleSearchByName }) => {
   const [value, setValue] = useState("");
